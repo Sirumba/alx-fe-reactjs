fe50291aedd02bd328206cab586a458498b4d157
Sirumba <simonsmaina45@gmail.com>
2025-11-02 19:23:42 +0300

    addednuserProfle.jsx and updated the app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,21 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
       <Header />
-      <MainContent /> 
+      <MainContent />
+      
+      {/* New component added here */}
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+      
       <Footer />
     </div>
   );
