48e9fea53832d8da64f6609f3d0ef063a2f63e2d
Sirumba <simonsmaina45@gmail.com>
2025-11-09 15:46:12 +0300

    deleted the my-company folder

--- my-company/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ textAlign: 'center', padding: '15px', backgroundColor: '#f1f1f1', marginTop: '20px' }}>
-      © 2023 My Company. All rights reserved.
-    </footer>
-  );
-}
-
-export default Footer;
