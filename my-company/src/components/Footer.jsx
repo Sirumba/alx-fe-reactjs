24476965e9ab7864a86c0e79f327cda70cbf8581
Sirumba <simonsmaina45@gmail.com>
2025-11-09 15:59:22 +0300

    Done with footer implimentations

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '15px', backgroundColor: '#f1f1f1', marginTop: '20px' }}>
+      © 2023 My Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
