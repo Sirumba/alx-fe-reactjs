389f40305d74b3eb1f7078666cd4a78df7ba85d8
Sirumba <simonsmaina45@gmail.com>
2025-12-11 22:37:09 +0300

    changed the states logic to fit the assignment design

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,29 +1,21 @@
 import { useState } from "react";
 
 function RegistrationForm() {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!formData.username || !formData.email || !formData.password) {
+    if (!username || !email || !password) {
       setError("All fields are required!");
       return;
     }
 
     setError("");
-
-    console.log("Submitted:", formData);
+    console.log({ username, email, password });
     alert("User registered successfully (Controlled Form)");
   };
 
@@ -38,28 +30,25 @@ function RegistrationForm() {
 
       <input
         type="text"
-        name="username"
         placeholder="Username"
-        value={formData.username}
-        onChange={handleChange}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
         className="border p-2 w-full"
       />
 
       <input
         type="email"
-        name="email"
         placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         className="border p-2 w-full"
       />
 
       <input
         type="password"
-        name="password"
         placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         className="border p-2 w-full"
       />
 
